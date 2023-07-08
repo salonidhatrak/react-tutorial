ad627a138ddef7b61b2f72f91278dfb5217efd9b
salonidhatrak <salonidhatrak123@gmail.com>
2023-07-08 19:57:31 +0530

    dark mode

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 // import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState("dark")
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark")
+    }
+    else {
+      setMode("light")
+    }
+  }
   return (
     <>
-      <Navbar title="textUtils1"/>
+      <Navbar title="textUtils1" mode={mode} toggleMode={toggleMode} />
       <div className='container'>
-      {/* <TextForm heading="Analyase the Text"/> */}
-      <About/>
+        {/* <TextForm heading="Analyase the Text"/> */}
+        <About />
       </div>
     </>
   );
--- react-practice/src/components/Navbar.js
+++ react-practice/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className={`navbar navbar-expand-lg navbar ${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,6 +17,10 @@ export default function Navbar(props) {
               <a className="nav-link" href="/">About</a>
             </li>
           </ul>
+          <div className="form-check form-switch">
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark</label>
+          </div>
         </div>
       </div>
     </nav>
