cd63965a122a80bdbc0ae9996dabb5e934e5001b
salonidhatrak <salonidhatrak123@gmail.com>
2023-07-08 19:12:25 +0530

    accordian and buttons in bootstrap

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="textUtils1"/>
       <div className='container'>
-      <TextForm heading="Analyase the Text"/>
+      {/* <TextForm heading="Analyase the Text"/> */}
+      <About/>
       </div>
     </>
   );
--- react-practice/src/components/TextForm.js
+++ react-practice/src/components/TextForm.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-       let newText=text.toUpperCase()
-       setText(newText)
+        let newText = text.toUpperCase()
+        setText(newText)
     }
 
     const hndleOnChange = (event) => {
@@ -12,13 +12,20 @@ export default function TextForm(props) {
     }
     const [text, setText] = useState("Enter Text here!!")
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <label className="form-label">Example textarea</label>
-                <textarea className="form-control" value={text} id="mybox" rows="6" onChange={hndleOnChange}>{props.heading}</textarea>
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <label className="form-label">Example textarea</label>
+                    <textarea className="form-control" value={text} id="mybox" rows="6" onChange={hndleOnChange}>{props.heading}</textarea>
+                </div>
+                <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
-        </div>
+
+            <div className='container my-2'>
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length} words and {text.length} Characters</p>
+            </div>
+        </>
     )
 }
