1cbebe3496722c27442778fb413c495dd5c2b71b
salonidhatrak <salonidhatrak123@gmail.com>
2023-08-02 10:36:04 +0530

    alert

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,27 +1,45 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+import TextForm from './components/TextForm';
 
 function App() {
   const [mode, setMode] = useState("dark")
+  const [alert, setAlert]=useState(null)
+
+const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    type:type
+  })
+}
+
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark")
+      showAlert("Dark mode", "success")
     }
     else {
       setMode("light")
+      showAlert("Light mode", "success")
     }
   }
   return (
     <>
       <Navbar title="textUtils1" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
       <div className='container'>
-        {/* <TextForm heading="Analyase the Text"/> */}
-        <About />
+        <TextForm heading="Analyase the Text"/>
+        {/* <About /> */}
       </div>
     </>
+
+    
+    
+    
   );
 }
 
